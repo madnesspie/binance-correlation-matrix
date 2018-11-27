b8eb33739a26697fe700322e3a213066cd9053a7
alexpl52 <apolishchuk52@gmail.com>
2018-11-28 05:00:08 +0700

    Concat downsampled tickdata

--- main.py
+++ main.py
@@ -1,26 +1,61 @@
 from os import listdir
 from itertools import permutations
 
-# import pandas
+from pandas import read_csv, concat
+from matplotlib import pyplot
+
+from logger import log, get_logger
 
 PATH = 'data/'
 CURRENCIES = ['btc', 'eth', 'bnb', 'xrp', 'xlm', 'ada', 'ltc', 'dash', 'xmr', 
               'zec', 'etc', 'neo', 'doge', 'mkr', 'omg', 'zrx', 'dcr', 'qtum']
 
+logger = get_logger(__name__)
+
 
 def get_tickers(currencies=CURRENCIES):
-    return tuple(map(lambda p: ''.join(p), permutations(currencies, 2)))
+    return [''.join(pair) for pair in permutations(currencies, 2)]
 
 
-def get_filenames(path=PATH):
-    return tuple(map(lambda f: f.replace('.csv', ''), listdir(path)))
+def filter_tickers(tickers):
+    return filter(lambda f: f.replace('.csv', '') in tickers, listdir(PATH))
 
 
-def filter_tickers(tickers, filenames):
-    tickers = get_tickers()
-    filenames = get_filenames()
-    return sorted(filter(lambda f: f in tickers, filenames))
+def to_dataframe(csv):
+    df = read_csv(f"{PATH}/{csv}", index_col=0, parse_dates=[['Date', 'Time']])
+    df['Ticker'] = csv.replace('.csv', '')
+    return df
 
+
+def resample(df):
+    df['Open'] = df.Open.resample('H').first()
+    df['High'] = df.High.resample('H').max()
+    df['Low'] = df.Low.resample('H').min()
+    df['Close'] = df.Close.resample('H').min()
+    df['Volume'] = df.Volume.resample('H').sum()
+    return df.dropna()
+
+
+def build_df(tickers):
+    dfs = map(to_dataframe, tickers)
+    downsampled_dfs = map(resample, dfs)
+    return concat(downsampled_dfs)
+
+    # if 'adabnb.csv' != ticker:
+    #     continue
+    # print(downsampled)
+    # df.plot()
+    # pyplot.show()
+    # break
+
+
+def main():
+    tickers = filter_tickers(get_tickers())
+    df = build_df(tickers)
+    print(df)
+    
+if __name__ == '__main__':
+    main()
 
 # class ParseTickers:
 #     def __init__(self):
